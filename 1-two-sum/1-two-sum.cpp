e870f13d3b360fc452eb5d4c65152de41b96d135
onitiucosmin <48385722+onitiucosmin@users.noreply.github.com>
2022-05-12 09:11:39 +0200

    Time: 17 ms (63.19%), Space: 11.2 MB (21.65%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        bool found = false;
-        for(auto i = 0; i < nums.size() - 1; i++){
-            for(auto j = i + 1; j < nums.size(); j++){
-                if(nums[i] + nums[j] == target) {
-                    return {i,j};  
-                }
+        std::map<int, int> map;
+        for(auto i = 0; i < nums.size(); i++) {
+            auto complement = target - nums[i];
+            if(map.find(complement) != map.end()){
+                return{map[complement],i}; 
             }
+            map[nums[i]] = i;
         }
+        
         return {0,0};
     }
 };
