5ebe16fbd8289d449c167ef8f7874c8de15b24c5
onitiucosmin <48385722+onitiucosmin@users.noreply.github.com>
2022-08-22 10:38:35 +0300

    Time: 110 ms (12.94%), Space: 42 MB (66.37%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -3,16 +3,5 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    var i = s.length-1;
-    var length = 0;
-    while(i >= 0){
-        if(s[i] != " "){ //in the top while loop we move from the back until we get a character which isn't a space
-            while(i >= 0 && s[i] != " "){ //at this point we have the last letter of the last word. length += 1 until we hit another space or the string is done
-                length += 1;
-                i -= 1;
-            }
-            return length;
-        }
-        i -= 1;
-    }
+    return (s.match(/(\w+)\s*$/) || [, ''])[1].length;
 };
