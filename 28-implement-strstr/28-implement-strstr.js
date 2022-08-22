762b60efa845bf4453adfc8d52347cb9a11105bb
onitiucosmin <48385722+onitiucosmin@users.noreply.github.com>
2022-08-22 10:33:36 +0300

    Time: 65 ms (90.81%), Space: 41.5 MB (97.07%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -4,16 +4,13 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    if (!needle) return 0
-      let idx = 0
-      for (let i = 0; i < haystack.length; i++) {
-        if (haystack[i] != needle[idx]) {
-          i = i-idx
-          idx = 0
-        } else {
-          if (idx == needle.length-1) return i-idx
-          idx++
+    if (needle.length == 0) return 0;
+    for (let i = 0; i < haystack.length; i++) {
+        let k = i, j = 0;
+        while (haystack[k] == needle[j] && j < needle.length) {
+            k++, j++;
         }
-      }
-      return -1
+        if (j == needle.length) return i;
+    }
+    return -1; // couldn't find needle in haystack
 };
